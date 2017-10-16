35b70ec3cd891527acd627059a36ae1fc28ddd9c
Mike Gasparelli <magasparel@gmail.com>
2017-10-15 20:58:54 -0400

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace url_repro.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public int PageNum { get; set; }
+
+        public async Task OnGetAsync([FromRoute] int pageNum = 1)
+        {
+            PageNum = pageNum;
+        }
+    }
+}
